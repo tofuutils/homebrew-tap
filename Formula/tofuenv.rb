a0552d113f8228ee50a4636db081c970c4da9906
Alexander Sharov <kvendingoldo@yandex.ru>
2024-01-03 15:41:58 +0100

    fix: homebrew formula

--- Formula/tofuenv.rb
+++ Formula/tofuenv.rb
@@ -1,18 +1,36 @@
 class Tofuenv < Formula
   desc "OpenTofu version manager inspired by tfenv"
   homepage "https://github.com/tofuutils/tofuenv"
-
   url "https://github.com/tofuutils/tofuenv/archive/refs/tags/v1.0.0.tar.gz"
-  version "1.0.0"
   # sha256 "TODO"
   license "MIT"
+  head "https://github.com/tofuutils/tofuenv.git", branch: "main"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+#   bottle do
+#     sha256 cellar: :any_skip_relocation, all: "4905c2390b0254348be44da1c4a05b3d8bf4d8704b94d16b739d64fd4709784b"
+#   end
+
+  uses_from_macos "unzip"
+
+  on_macos do
+    depends_on "grep"
+  end
+
+  conflicts_with "opentofu", because: "tofuenv symlinks opentofu binaries"
 
   def install
-    libexec.install Dir["*"]
-    bin.install_symlink "#{libexec}/bin/quarkus" => "quarkus"
+    prefix.install %w[bin lib libexec share]
   end
 
   test do
-    system "#{bin}/tofuenv", "help"
+    assert_match "1.6.0-rc1", shell_output("#{bin}/tofuenv list-remote")
+    with_env(TOFUENV_TOFU_VERSION: "1.6.0-rc1", TF_AUTO_INSTALL: "false") do
+      assert_equal "1.6.0-rc1", shell_output("#{bin}/tofuenv version-name").strip
+    end
   end
 end
